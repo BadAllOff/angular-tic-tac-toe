f750bb9dd5bb7a0935de45807e4be4b73fe4ae1c
Shamil <shamil.badalloffsh@gmail.com>
2020-03-20 17:47:09 +0100

    Board ooks and works ok

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -5,8 +5,10 @@ import { Component, OnInit, Input } from '@angular/core';
   template: `
   <button>{{ value }}</button>  
   `,
-  styles: []
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
+
   @Input() value: 'X' | 'O';
+
 }
